3f7afbe59aba2947d651f4baf642579e2abf37e3
IMMORTL225 <lilsci2013@gmail.com>
2026-03-08 15:27:30 -0700

    Commit and push matthew's ?th commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/matthewtest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class matthewtest {
+private DcMotor leftFront;
+private DcMotor rightFront;
+private DcMotor leftBack;
+private DcMotor rightBack;
+private DcMotor Flywheel;
+
+public void  init(HardwareMap hwMap){
+    Flywheel = hwMap.get(DcMotor.class, "Flywheel");
+    Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+}
+}
