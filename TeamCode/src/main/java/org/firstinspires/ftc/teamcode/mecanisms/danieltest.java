9ecc82526352a6a3ddd8dee6ca1e8022851e8a44
danielthai <daniel.thai2011@gmail.com>
2026-03-08 15:38:16 -0700

    This is Daniel's Starterbotteleopmecanums for java push.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanisms/danieltest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.mecanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class danieltest {
+    private DcMotor Flywheel;
+    private DcMotor rightFront;
+    private DcMotor leftFront;
+    private DcMotor rightBack;
+    private DcMotor leftBack;
+
+
+    public void init(HardwareMap hwMap) {
+        Flywheel = hwMap.get(DcMotor.class, "Flywheel");
+        Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }}
