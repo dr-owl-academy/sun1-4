d53dd3fc2d38f153e8007f6c1cac5e115e6d1294
IMMORTL225 <lilsci2013@gmail.com>
2026-03-08 15:38:34 -0700

    Commit and push matthew's ?th commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/matthewtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/matthewtest.java
@@ -14,5 +14,13 @@ public void  init(HardwareMap hwMap){
     Flywheel = hwMap.get(DcMotor.class, "Flywheel");
     Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightFront = hwMap.get(DcMotor.class, "rightFront");
+    rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightBack = hwMap.get(DcMotor.class, "rightBack");
+    rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftFront = hwMap.get(DcMotor.class, "leftFront");
+    leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftBack = hwMap.get(DcMotor.class, "leftBack");
+    leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 }
 }
