a72a1fbbd6fe77f1706ab35c1b0185ec6c1dea27
SomeGuyOnline66 <duyifei5846@students.esuhsd.org>
2026-03-15 15:03:01 -0700

    deleted everyones code and left my own + cleaned up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AimeeMechanismsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AimeeMechanismsTest.java
@@ -1,27 +1,26 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@TeleOp
-public class AimeeMechanismsTest extends OpMode {
-
+public class AimeeMechanismsTest {
     private DcMotor Flywheel;
     private DcMotor rightFront;
     private DcMotor leftFront;
     private DcMotor rightBack;
     private DcMotor leftBack;
 
+
     @Override
-    public void init(HardwareMap hwMap) {
-        Flywheel = hwMap.get(DcMotor .class,"Flywheel");
+    public void init(HardwareMap hwMap){
+        Flywheel = hwMap.get(DcMotor.class, "Flywheel");
         Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront = hwMap.get(DcMotor .class,"rightFront");
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack = hwMap.get(DcMotor.class, "rightBack");
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -30,15 +29,44 @@ public class AimeeMechanismsTest extends OpMode {
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack = hwMap.get(DcMotor.class, "leftBack");
         leftBack.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
     }
+
 
     @Override
     public void loop() {
+        if (gamepad1.x){
+            leftFront.setPower(0.3);
+        } else {
+            leftFront.setPower(0.0);
+        }
 
-if (gamepad1.y){
-    rightFront.setPower(0.3);
-}
+
+        if (gamepad1.y){
+            rightFront.setPower(0.3);
+        } else {
+            rightFront.setPower(0.0);
+        }
+
+
+        if (gamepad1.a){
+            leftBack.setPower(0.3);
+        } else {
+            leftBack.setPower(0.0);
+        }
+
+
+        if (gamepad1.b){
+            rightBack.setPower(0.3);
+        } else {
+            rightBack.setPower(0.0);
+        }
+
+
     }
-
 }
+
+
+
+
