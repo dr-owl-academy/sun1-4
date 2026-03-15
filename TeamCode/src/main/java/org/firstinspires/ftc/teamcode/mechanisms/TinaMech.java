c3af8b733a084157294ad24345f86af2fe08edcb
tinazhang <tina.jl.zhang@gmail.com>
2026-03-14 20:17:24 -0700

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TinaMech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TinaMech.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -12,29 +15,42 @@ public class TinaMech {
     private DcMotor leftBack;
 
 
-
-
     @Override
     public void init(HardwareMap hwMap){
-        Flywheel = hwMap.get(DcMotor.class,"Flywheel");
+        Flywheel = hwMap.get(DcMotor.class, "Flywheel");
         Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront = hwMap.get(DcMotor.class, "rightFront");
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack = hwMap.get((DcMotor.class, "rightBack");
-        rightBack.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront = hwMap.get(DcMotor.class, "leftFront");
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack = hwMap.get((DcMotor.class, "leftBack");
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
         leftBack.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
 
 
     @Override
     public void loop() {
+
+        init(hardwareMap);
+
+        if (gamepad1.y) {
+            rightFront.setPower(0.5);
+        }
+        if (gamepad1.a) {
+            rightBack.setPower(0.5);
+        }
+        if (gamepad1.x) {
+            leftFront.setPower(0.5);
+        }
+        if (gamepad1.b) {
+            leftBack.setPower(0.5);
+        }
 
     }
 }
