35ba3136126ea5df677fe4cab166ae59403b54e3
SomeGuyOnline66 <duyifei5846@students.esuhsd.org>
2026-03-22 14:58:23 -0700

    added left and right transfers + the telemetry for right and left transfers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AimeeMechanismsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AimeeMechanismsTest.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gam
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -15,22 +16,28 @@ public class AimeeMechanismsTest extends OpMode {
     private DcMotor rightBack;
     private DcMotor leftBack;
 
+    private CRServo leftTransfer;
+
+    private CRServo rightTransfer;
+
 
     @Override
     public void init(){
         Flywheel = hardwareMap.get(DcMotor.class, "Flywheel");
         Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightFront = hardwareMap.get(DcMotor.class, "frontRight");
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack = hardwareMap.get(DcMotor.class, "backRight");
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack = hardwareMap.get(DcMotor.class, "backLeft");
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftTransfer = hardwareMap.get(CRServo.class, "leftTransfer");
+        rightTransfer = hardwareMap.get(CRServo.class, "rightTransfer");
 
     }
 
@@ -70,11 +77,27 @@ public class AimeeMechanismsTest extends OpMode {
             Flywheel.setPower(0.0);
         }
 
+        if (gamepad1.dpad_left) {
+            leftTransfer.setPower(0.5);
+        } else {
+            leftTransfer.setPower(0.0);
+        }
+
+        if(gamepad1.dpad_right) {
+            rightTransfer.setPower(0.5);
+        } else {
+            rightTransfer.setPower(0.0);
+        }
+
+        /** one of the Transfers has to be in reverse I think so add that later **/
+
         telemetry.addData("RF", rightFront.getPower());
         telemetry.addData("LF", leftFront.getPower());
         telemetry.addData("LB", leftBack.getPower());
         telemetry.addData("RB", rightBack.getPower());
         telemetry.addData("Flywheel", Flywheel.getPower());
+        telemetry.addData("leftTransfer", leftTransfer.getPower());
+        telemetry.addData("rightTransfer", rightTransfer.getPower());
         telemetry.update();
 
     }
