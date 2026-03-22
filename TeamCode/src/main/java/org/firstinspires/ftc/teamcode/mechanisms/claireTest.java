a58db1bf5044c8fdf44b59a3ef5053f33f8a64cf
hungfu-panda <claire.huangfu007@gmail.com>
2026-03-22 12:49:34 -0700

    claire mar 22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/claireTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/claireTest.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="claireTest", group="Test")
 
-public class claireTest extends LinearOpMode {
+public class claireTest extends OpMode {
 
     DcMotor rightFront;
     DcMotor rightBack;
@@ -15,52 +15,50 @@ public class claireTest extends LinearOpMode {
     DcMotor Flywheel;
 
     @Override
-    public void runOpMode() {
-
+    public void init() {
         rightFront = hardwareMap.get(DcMotor.class, "frontRight");
         rightBack = hardwareMap.get(DcMotor.class, "backRight");
         leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
         leftBack = hardwareMap.get(DcMotor.class, "backLeft");
         Flywheel = hardwareMap.get(DcMotor.class, "Flywheel");
+    }
 
-        waitForStart();
+    @Override
+    public void loop() {
 
-        while (opModeIsActive()) {
+        // A button → rightFront
+        if (gamepad1.a) {
+            rightFront.setPower(0.3);
+        } else {
+            rightFront.setPower(0);
+        }
 
-            // A button → rightFront
-            if (gamepad1.a) {
-                rightFront.setPower(0.3);
-            } else {
-                rightFront.setPower(0);
-            }
+        // B button → rightBack
+        if (gamepad1.b) {
+            rightBack.setPower(0.3);
+        } else {
+            rightBack.setPower(0);
+        }
 
-            // B button → rightBack
-            if (gamepad1.b) {
-                rightBack.setPower(0.3);
-            } else {
-                rightBack.setPower(0);
-            }
+        // X button → leftFront
+        if (gamepad1.x) {
+            leftFront.setPower(0.3);
+        } else {
+            leftFront.setPower(0);
+        }
 
-            // X button → leftFront
-            if (gamepad1.x) {
-                leftFront.setPower(0.3);
-            } else {
-                leftFront.setPower(0);
-            }
+        // Y button → leftBack
+        if (gamepad1.y) {
+            leftBack.setPower(0.3);
+        } else {
+            leftBack.setPower(0);
+        }
 
-            // Y button → leftBack
-            if (gamepad1.y) {
-                leftBack.setPower(0.3);
-            } else {
-                leftBack.setPower(0);
-            }
-
-            // Right bumper → Flywheel
-            if (gamepad1.right_bumper) {
-                Flywheel.setPower(0.3);
-            } else {
-                Flywheel.setPower(0);
-            }
+        // Right bumper → Flywheel
+        if (gamepad1.right_bumper) {
+            Flywheel.setPower(0.3);
+        } else {
+            Flywheel.setPower(0);
         }
     }
 }
