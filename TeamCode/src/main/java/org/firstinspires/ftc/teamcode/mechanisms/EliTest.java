cc5c03d2ed3279baf08854b28c024c5875b06567
eliyao <yao.eli@icloud.com>
2026-03-22 14:16:46 -0700

    this is Eli's teleop check in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/EliTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/EliTest.java
@@ -2,10 +2,9 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class EliTest {
-    // your code here
-
 
     private DcMotor flywheel;
     private DcMotor rightFront;
@@ -14,43 +13,48 @@ public class EliTest {
     private DcMotor leftBack;
 
     // constants for encoder calculations
-    private static final double ticksPerRev = 537.6; // example for GoBilda motor
-    private static final double inPerTick = 0.01;    // adjust for your wheel setup
+    private static final double ticksPerRev = 537.6;
+    private static final double inPerTick = 0.01;
 
-
-    public void init(HardwareMap hwMap){
+    public void init(HardwareMap hwMap) {
 
         flywheel = hwMap.get(DcMotor.class, "Flywheel");
         flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         rightFront = hwMap.get(DcMotor.class, "rightFront");
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         leftFront = hwMap.get(DcMotor.class, "leftFront");
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         rightBack = hwMap.get(DcMotor.class, "rightBack");
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         leftBack = hwMap.get(DcMotor.class, "leftBack");
+
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Reverse left side for correct driving
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void setMotorSpeed(double speed) {
-        flywheel.setPower(speed);
-    }
+    // Call this in your OpMode loop
+    public void drive(Gamepad gamepad) {
 
-    public double getMotorRevs() {
-        return flywheel.getCurrentPosition() / ticksPerRev;
-    }
+        // Tank drive
+        double leftPower = -gamepad.left_stick_y;
+        double rightPower = -gamepad.right_stick_y;
 
-    public void rotateInInches(double distance) {
-        int distanceInTicks = (int) Math.round(distance / inPerTick);
+        leftFront.setPower(leftPower);
+        leftBack.setPower(leftPower);
+        rightFront.setPower(rightPower);
+        rightBack.setPower(rightPower);
 
-        flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        flywheel.setTargetPosition(distanceInTicks);
-        flywheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        flywheel.setPower(1.0);
+        // Flywheel control (press A to spin)
+        if (gamepad.a) {
+            flywheel.setPower(1.0);
+        } else {
+            flywheel.setPower(0);
+        }
     }
 }
+
