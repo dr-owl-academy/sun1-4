0c00e3386457da5b2441dad523c2c67819694e6a
IMMORTL225 <lilsci2013@gmail.com>
2026-03-27 18:00:20 -0700

    Commit and push matthew's ?th commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MatthewTeleopSterterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MatthewTeleopSterterBot.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.tuning;
+package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
