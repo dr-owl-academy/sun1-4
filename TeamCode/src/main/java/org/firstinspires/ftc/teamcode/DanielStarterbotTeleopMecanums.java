6edc98e4b548f60c6f44a944c0450cbea85a31aa
danielthai <daniel.thai2011@gmail.com>
2026-03-27 17:57:00 -0700

    Changed left wheel from reverse to forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
@@ -147,7 +147,7 @@ public class DanielStarterbotTeleopMecanums extends OpMode {
          */
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
         /*
