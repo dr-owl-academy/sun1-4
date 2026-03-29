e6855df4abba202a17c832de6932281bddf017c9
danielthai <daniel.thai2011@gmail.com>
2026-03-29 15:21:50 -0700

    Trying to fix Dpad #1 and changed increments from 100 to 50, and added and edited more telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
@@ -248,8 +248,8 @@ public class DanielStarterbotTeleopMecanums extends OpMode {
          */
         telemetry.addData("State", launchState);
         telemetry.addData("motorSpeed", launcher.getVelocity());
-        telemetry.addData("launcherminspeed", LAUNCHER_MIN_VELOCITY());
-        telemetry.addData("launchertargetspeed", LAUNCHER_TARGET_VELOCITY());
+        telemetry.addData("launcherminspeed", LAUNCHER_MIN_VELOCITY);
+        telemetry.addData("launchertargetspeed", LAUNCHER_TARGET_VELOCITY);
         telemetry.update();
     }
 
