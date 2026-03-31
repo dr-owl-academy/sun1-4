7b4e54e6abcf7d37bbdc86f40acc57d42ee1028a
hwgalaxyguardians <wanghmnet@gmail.com>
2026-03-30 19:07:29 -0700

    Hanming Mar 30 Ready for Apr 4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/HanmingOpModeTest.java
@@ -0,0 +1,177 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name = "HanmingOpModeTest", group = "StarterBot")
+public class HanmingOpModeTest extends OpMode {
+    final double FEED_TIME_SECONDS = 0.50;
+    final double STOP_SPEED = 0.0;
+    final double FULL_SPEED = 1.0;
+
+    double LAUNCHER_TARGET_VELOCITY = 2000;
+    double LAUNCHER_MIN_VELOCITY = 900;
+
+    // Declare OpMode members.
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotorEx launcher = null;
+    private CRServo leftFeeder = null;
+    private CRServo rightFeeder = null;
+
+    ElapsedTime feederTimer = new ElapsedTime();
+
+    private enum LaunchState {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHING,
+    }
+
+    private LaunchState launchState;
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
+
+    @Override
+    public void init() {
+        launchState = LaunchState.IDLE;
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontRight");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "backLeft");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "backRight");
+        launcher = hardwareMap.get(DcMotorEx.class, "Flywheel");
+        leftFeeder = hardwareMap.get(CRServo.class, "leftTransfer");
+        rightFeeder = hardwareMap.get(CRServo.class, "rightTransfer");
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontDrive.setZeroPowerBehavior(BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(BRAKE);
+        leftBackDrive.setZeroPowerBehavior(BRAKE);
+        rightBackDrive.setZeroPowerBehavior(BRAKE);
+        launcher.setZeroPowerBehavior(BRAKE);
+        leftFeeder.setPower(STOP_SPEED);
+        rightFeeder.setPower(STOP_SPEED);
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
+        rightFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void init_loop() {
+    }
+    @Override
+    public void start() {
+    }
+    @Override
+    public void loop() {
+        mecanumDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+
+        if (gamepad2.right_bumper) {
+            launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+        } else if (gamepad2.b) { // stop flywheel
+            launcher.setVelocity(STOP_SPEED);
+        }
+        if (gamepad2.dpadUpWasPressed()) {
+            LAUNCHER_TARGET_VELOCITY += 50;
+        }
+
+        if (gamepad2.dpadDownWasPressed()) {
+            LAUNCHER_TARGET_VELOCITY -= 50;
+        }
+
+        if (gamepad2.right_trigger > 0.1) {
+            leftFeeder.setPower(FULL_SPEED);
+            rightFeeder.setPower(FULL_SPEED);
+        } else {
+            leftFeeder.setPower(STOP_SPEED);
+            rightFeeder.setPower(STOP_SPEED);
+        }
+
+        telemetry.addData("motorSpeed", launcher.getVelocity());
+        telemetry.addData("Launch Min Vel", LAUNCHER_MIN_VELOCITY);
+        telemetry.addData("Launch tgt Vel", LAUNCHER_TARGET_VELOCITY);
+        telemetry.update();
+
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    void mecanumDrive(double forward, double strafe, double rotate){
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontPower = (forward + strafe + rotate) / denominator;
+        rightFrontPower = (forward - strafe - rotate) / denominator;
+        leftBackPower = (forward - strafe + rotate) / denominator;
+        rightBackPower = (forward + strafe - rotate) / denominator;
+
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+
+    }
+
+    void launch(boolean bumperHeld) {
+        switch (launchState) {
+
+            case IDLE:
+                if (bumperHeld) {
+                    launchState = LaunchState.SPIN_UP;
+                }
+                break;
+
+            case SPIN_UP:
+                if (!bumperHeld) {
+                    launchState = LaunchState.IDLE;
+                    break;
+                }
+
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+
+                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY) {
+                    feederTimer.reset(); // start delay timer
+                    launchState = LaunchState.LAUNCH;
+                }
+                break;
+
+            case LAUNCH:
+                if (!bumperHeld) {
+                    leftFeeder.setPower(STOP_SPEED);
+                    rightFeeder.setPower(STOP_SPEED);
+                    launchState = LaunchState.IDLE;
+                    break;
+                }
+
+                if (feederTimer.seconds() > 1.0) {
+                    leftFeeder.setPower(FULL_SPEED);
+                    rightFeeder.setPower(FULL_SPEED);
+                    launchState = LaunchState.LAUNCHING;
+                }
+                break;
+
+            case LAUNCHING:
+                if (!bumperHeld) {
+                    leftFeeder.setPower(STOP_SPEED);
+                    rightFeeder.setPower(STOP_SPEED);
+                    launchState = LaunchState.IDLE;
+                }
+                break;
+        }
+    }
+}
