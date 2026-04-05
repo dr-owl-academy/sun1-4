0f4345c50ad846fbb0b2bb88c53cc37a10a5731b
danielthai <daniel.thai2011@gmail.com>
2026-04-05 15:14:09 -0700

    Pinpoint offsets adjust #2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielPinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielPinpoint.java
@@ -84,7 +84,7 @@ public class DanielPinpoint extends OpMode {
          *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
          *  Forward of center is a positive number, backwards is a negative number.
          */
-        pinpoint.setOffsets(-55.0, -110.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinpoint.setOffsets(55.0, 110.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
          * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
