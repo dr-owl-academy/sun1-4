b909c76a145662f1cf705a52b55184c239470eac
IMMORTL225 <lilsci2013@gmail.com>
2026-04-05 15:04:08 -0700

    Commit and push matthew's ?th commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MatthewPinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MatthewPinpoint.java
@@ -84,7 +84,7 @@ public class MatthewPinpoint extends OpMode {
          *  The Y pod offset refers to how far forwards from the tracking point the Y (strafe) odometry pod is.
          *  Forward of center is a positive number, backwards is a negative number.
          */
-        pinpoint.setOffsets(0, -20, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinpoint.setOffsets(-20, -20, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
          * Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
