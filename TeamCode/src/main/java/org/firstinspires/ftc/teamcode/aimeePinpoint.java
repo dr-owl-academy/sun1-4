0c906413c05ae9ede06a0c5cdd7d08d96dd4ffbe
SomeGuyOnline66 <duyifei5846@students.esuhsd.org>
2026-04-05 14:53:03 -0700

    changed package to teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aimeePinpoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aimeePinpoint.java
@@ -19,7 +19,7 @@
  *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *   SOFTWARE.
  */
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
