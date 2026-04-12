2e18ab824531bd1ec70d39948797061c9404c4f4
SomeGuyOnline66 <duyifei5846@students.esuhsd.org>
2026-04-12 15:51:03 -0700

    imported in pose2d and poseVelocity2d to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimeeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AimeeTeleop.java
@@ -35,6 +35,8 @@ package org.firstinspires.ftc.teamcode;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
