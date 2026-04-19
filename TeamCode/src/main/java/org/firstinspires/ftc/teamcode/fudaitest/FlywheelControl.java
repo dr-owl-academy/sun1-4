37b6087798be977b73c8e763d3442097cc93b65d
Fudai <fudyyy169@gmail.com>
2026-04-19 15:07:58 -0700

    code clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fudaitest/FlywheelControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fudaitest/FlywheelControl.java
@@ -1,34 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.fudaitest;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class FudaiTest {
-    private DcMotor flyWheel;
-    private DcMotor rightFront;
-    private DcMotor leftFront;
-    private DcMotor rightBack;
-    private DcMotor leftBack;
 
-
-    public void init(HardwareMap hwMap) {
-        flyWheel = hwMap.get(DcMotor.class, "Flywheel");
-        flyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        flyWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront = hwMap.get(DcMotor.class, "rightFront");
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack = hwMap.get(DcMotor.class, "rightBack");
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront = hwMap.get(DcMotor.class, "leftFront");
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack = hwMap.get(DcMotor.class, "leftBack");
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-}
-package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
