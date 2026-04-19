771de626dfd84d21529f6d55d6040793b23ceac1
tinazhang <tina.jl.zhang@gmail.com>
2026-04-19 14:23:42 -0700

    fixing stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TinaTeleopStarterbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TinaTeleopStarterbot.java
@@ -356,7 +356,7 @@ public class TinaTeleopStarterbot extends OpMode {
 
         x = Math.max(18, x);
 
-        return - 0.00235435 * x * x * x - 0.656851 * x * x + 63.00581 * x - 568.02224;
+        return - 0.000182489 * x * x * x - 0.0676779 * x * x + 13.51296 * x + 739.6681;
 
     }
 }
