d2a4b9b96fcd35e17b8f51fe56c3b811c4b4e88e
danielthai <daniel.thai2011@gmail.com>
2026-04-23 17:36:50 -0700

    Spin code attempt #3 (Changed spin to Gamepad 2 right bumper) and correcting drive direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DanielStarterbotTeleopMecanums.java
@@ -160,7 +160,7 @@ public class DanielStarterbotTeleopMecanums extends OpMode {
          */
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
         /*
